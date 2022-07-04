4668b8da2f8b9976128cd0fe15bb69fc425136f4
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-07-04 20:46:05 -0300

    Testes end-to-end com JAX-RS e Grizzly

---
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -0,0 +1,69 @@
+package test.java.br.com.alura.loja;
+
+import com.thoughtworks.xstream.XStream;
+import main.java.br.com.alura.loja.Servidor;
+import main.java.br.com.alura.loja.modelo.Carrinho;
+import main.java.br.com.alura.loja.modelo.Projeto;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+public class ClienteTest {
+
+    private HttpServer server;
+
+    @Before
+    public void startaServidor() {
+        server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
+
+    @Test
+    public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+
+    @Test
+    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/carrinhos").request().get(String.class);
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+    }
+
+    @Test
+    public void testaQueAConexaoComOServidorFunciona() {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://www.mocky.io");
+        String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+        Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+
+    }
+
+    @Test
+    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos").request().get(String.class);
+        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+
+
+    }
+
+}
